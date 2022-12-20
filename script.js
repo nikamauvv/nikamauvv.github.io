41692e833293dcf229ba87eb0c18d10c656b00df
nikamauvv <116631803+nikamauvv@users.noreply.github.com>
2022-12-20 12:39:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,263 @@
-let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
-  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
-  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
-  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
-  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
-  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
-  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
-  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
-  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' },
-  {text : 'Потанцевать' , image :  'https://code.s3.yandex.net/web-code/procrastinate/1.gif'}
-];
+var canvas = document.getElementById('c1');
+var ctx = canvas.getContext('2d');
+var mas = [];
+var count = 0;
+var stop = 1;
+var level =0;
+var population = 0;
+var work = 1;
+ctx.fillStyle = "rgba(255,255,255, 0.7)";
+var  pause=0;
+var time= 700;
 
-function getRandomElement(arr) {
-  let randIndex = Math.floor(Math.random() * arr.length);
-  return arr[randIndex];
+canvas.onclick = function (event) {
+  var x = event.offsetX;
+  var y = event.offsetY;
+  //console.log(x);
+  //console.log(y);
+  x = Math.floor(x / 10);
+  y = Math.floor(y / 10);
+  mas[y][x] = 1;
+  //console.log(y,x);
+  //console.log(mas);
+  drawField();
+};
+function goLife() {
+  var n = 70,
+    m = 70;
+  for (var i = 0; i < m; i++) {
+    mas[i] = [];
+    for (var j = 0; j < n; j++) {
+      mas[i][j] = [0];
+    }
+  }
+}
+goLife();
+
+function pousetimeFunc() {
+  if (pause==0){pause=1;}
+  else{pause=0;}
+  startLife();
 }
 
-let button = document.querySelector('.button');
-let phrase = document.querySelector('.phrase');
-let advice = document.querySelector('.advice');
-let image = document.querySelector('.image');
 
-button.addEventListener('click', function () {
-  let randomElement = getRandomElement(phrases);
-  smoothly(phrase, 'textContent', randomElement.text);
-  smoothly(image, 'src', randomElement.image);
 
-  if (randomElement.text.length > 40) {
-    advice.style.fontSize = '33px';
-  } else {
-    advice.style.fontSize = '42px';
+
+function clearField() {
+    level=0;
+    work=1;
+  ctx.clearRect(0, 0, 700, 700);
+  for (var i = 0; i < 70; i++) {
+    for (var j = 0; j < 70; j++) {
+      mas[i][j] = [0];
+    }
   }
-});
-for (let i = 0; i <= 2; i = i + 1) {
-smoothly(phrase, 'textContent', phrases[i].text);
-  smoothly(image, 'src', phrases[i].image); 
+  count = 0;
+  document.getElementById('count').innerHTML = count;
+  stop = 0;
+  population = 0;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML = 'EMPTY';
+}
+
+function drawField() {
+  ctx.clearRect(0, 0, 700, 700);
+
+  for (var i = 0; i < 70; i++) {
+    for (var j = 0; j < 70; j++) {
+      if (mas[i][j] == 1) {
+        ctx.fillRect(j * 10, i * 10, 10, 10);
+      }
+    }
+  }
+}
+function startLife() {
+
+ drawField();
+  var mas2 = [];
+
+  for (var i = 0; i < 70; i++) {
+    mas2[i] = [];
+
+    for (var j = 0; j < 70; j++) {
+      var neighbors = 0;
+      if (mas[fpm(i) - 1][j] == 1) neighbors++;
+      if (mas[i][fpp(j) + 1] == 1) neighbors++;
+      if (mas[fpp(i) + 1][j] == 1) neighbors++;
+      if (mas[i][fpm(j) - 1] == 1) neighbors++;
+      if (mas[fpm(i) - 1][fpp(j) + 1] == 1) neighbors++;
+      if (mas[fpp(i) + 1][fpp(j) + 1] == 1) neighbors++;
+      if (mas[fpp(i) + 1][fpm(j) - 1] == 1) neighbors++;
+      if (mas[fpm(i) - 1][fpm(j) - 1] == 1) neighbors++;
+
+      if (mas[i][j] ==1 && neighbors < 2) mas2[i][j] = 0;
+      if (mas[i][j] ==1 && neighbors > 3) mas2[i][j] = 0;
+      if (mas[i][j] ==1 && neighbors==2 || neighbors==3  ) mas2[i][j] = 1;
+      if (mas[i][j] ==0 && neighbors == 3) mas2[i][j] = 1;
+    }
+    
+    
+  }
+  mas = mas2;
+  
+  count++;
+  if (level == 1) {
+    check1level();
+  }
+  if (level == 2) {
+    check2level();
+  }
+  if (level == 3) {
+    check3level();
+  }
+  if (level == 4) {
+    check4level();
+  }
+  if (level == 5) {
+    check5level();
+  }
+  count_population();
+  document.getElementById('count').innerHTML = count;
+  document.getElementById('popul').innerHTML = population;
+  if (work==1 && pause==0) 
+  {timer = setTimeout(startLife, time);}
+
+}
+function fpm(i) {
+  if (i == 0) return 70;
+  else return i;
+}
+function fpp(i) {
+  if (i == 69) return -1;
+  else return i;
+}
+function stopLife() {
+    work = 0;
+    
+  //console.log(stop);
+}
+
+function level1() {
+  mas[14][15] = 1;
+  mas[16][15] = 1;
+  level = 1;
+  population = 2;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML =
+    'Размер популяции должен быть равен 3 к 10 поколению';
+  drawField();
+}
+function level2() {
+  mas[36][32] = 1;
+  mas[37][32] = 1;
+  level = 2;
+  population = 2;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML =
+    'Размер популяции должен быть равен 4 к 10 поколению';
+  drawField();
+}
+function level3() {
+  mas[10][10] = 1;
+  mas[10][11] = 1;
+  mas[61][10] = 1;
+  mas[60][10] = 1;
+  mas[61][50] = 1;
+  mas[61][51] = 1;
+  mas[10][61] = 1;
+  mas[11][61] = 1;
+  level = 3;
+  population = 8;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML =
+    'Размер популяции должен быть равен 16 к 10 поколению';
+  drawField();
+}
+function level4() {
+  level = 4;
+  population = 0;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML =
+    'Размер популяции должен быть равен 3 к 10 поколению';
+  drawField();
+}
+function level5() {
+  mas[29][25] = 1;
+  mas[30][25] = 1;
+  mas[29][26] = 1;
+  mas[30][26] = 1;
+  level = 5;
+  population = 4;
+  document.getElementById('popul').innerHTML = population;
+  document.getElementById('info').innerHTML =
+    'Размер популяции должен быть равен 0 к 5 поколению';
+  drawField();
+}
+
+function count_population() {
+  population=0;
+  for (var i = 0; i < 70; i++) {
+    for (var j = 0; j < 70; j++) {
+      if (mas[i][j] == 1) {
+        population++;
+      }
+    }
+  }
+  //console.log(population);
+}
+
+function check1level() {
+  count_population();
+  
+  if (count == 10 && population == 3) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'You score';
+  } else if (count > 10 && population != 3) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'Didn’t work, Try again';
+  }
+}
+function check2level() {
+  count_population();
+  
+  if (count == 10 && population == 4) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'You score';
+  } else if (count > 10 && population != 4) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'Didn’t work, Try again';
+  }
+}
+function check3level() {
+  count_population();
+  
+  if (count == 10 && population == 16) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'You score';
+  } else if (count > 10 && population != 16) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'Didn’t work, Try again';
+  }
+}
+function check4level() {
+  count_population();
+  
+  if (count == 10 && population == 3) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'You score';
+  } else if (count > 10 && population != 3) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'Didn’t work, Try again';
+  }
+}
+function check5level() {
+  count_population();
+  
+  if (count == 5 && population == 0) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'You score';
+  } else if (count > 5 && population != 0) {
+    stopLife();
+    document.getElementById('info').innerHTML = 'Didn’t work, Try again';
+  }
 }
